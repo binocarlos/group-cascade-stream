63a98479913ef6612adfcaa26161eb9558771691
Kai Davenport <kaiyadavenport@gmail.com>
2014-04-21 21:44:47 +0000

    test for external add

--- test/test.js
+++ test/test.js
@@ -54,6 +54,60 @@ test('cascade streams from single input in object mode', function(t) {
   source.pipe(pipeline).pipe(sink)
 })
 
+
+test('external add', function(t) {
+
+  var coeffs = {
+    a:2,
+    b:5,
+    c:10
+  }
+
+  var ends = 0
+  // create a stream for a single letter
+  function createLetterStream(letter){
+    var coeff = coeffs[letter]
+    return through.obj(function(chunk, enc, cb){
+      var num = parseFloat(chunk.replace(/\D/g, ''))
+      this.push(num * coeff)
+      cb()
+    }, function(){
+      ends++
+    })
+  }
+
+  var s = ['a1', 'b3', 'a4', 'c3', 'c8', 'b5']
+  var source = from.obj(s)
+
+  var pipeline = cascade.obj(function(chunk){
+    // return just 'a' for the id
+    return chunk.toString().charAt(0)
+  }, function(id){
+    return createLetterStream(id)
+  })
+
+  var results = [];
+  var sink = through.obj(function(chunk, enc, cb){
+    results.push(chunk)
+    if(chunk==8){
+      pipeline.add(from.obj([100,101]))
+    }
+    cb()
+  }, function(){
+
+    t.equal(ends, 3)
+
+    t.equal(results.length, s.length+2)
+
+    t.deepEqual(results, [2,15,8,100,101,30,80,25])
+
+
+    t.end()
+  })
+
+  source.pipe(pipeline).pipe(sink)
+})
+
 test('with no input', function(t) {
 
   var coeffs = {
